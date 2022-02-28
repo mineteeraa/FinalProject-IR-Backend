7a4fd449a5c3ea96be707189953ff724e596cbe2
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2022-02-28 23:19:20 +0700

    get data by input name using TFIDF

--- search_recipes.py
+++ search_recipes.py
@@ -2,20 +2,28 @@ import string
 
 import numpy as np
 import pandas as pd
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import re
 
 def get_and_clean():
     readFile = pd.read_csv('resource/Food Ingredients and Recipe Dataset with Image Name Mapping.csv')
     readFile = pd.DataFrame(readFile)
-    search_for_recipe_by_name(readFile)
+    readFile = readFile.dropna()
+    readFile = readFile.drop_duplicates()
+    for i, row in readFile.iterrows():
+        readFile.at[i, 'Instructions'] = readFile.at[i, 'Instructions'].lower()
+        readFile.at[i, 'Instructions'] = readFile.at[i, 'Instructions'].translate(str.maketrans('', '', string.punctuation + u'\xa0'))
+        readFile.at[i, 'Instructions'] = readFile.at[i, 'Instructions'].translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), ''))
 
-def search_for_recipe_by_name(data_sec):
+    search_for_recipe_by_name_TFIDF(readFile)
+
+def search_for_recipe_by_name_TFIDF(data_sec):
     recipeName = data_sec
     for i, row in recipeName.iterrows():
         recipeName.at[i, 'Title'] = recipeName.at[i, 'Title'].lower()
         recipeName.at[i, 'Title'] = recipeName.at[i, 'Title'].translate(str.maketrans('', '', string.punctuation + u'\xa0'))
-        recipeName.at[i, 'Title'] = recipeName.at[i, 'Title'].translate(str.maketrans(string.whitespace, ' '*len(string.whitespace), ''))
-
+        recipeName.at[i, 'Title'] = recipeName.at[i, 'Title'].translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), ''))
 
     print("-------------------------------------------")
     print("input name: ")
@@ -24,19 +32,21 @@ def search_for_recipe_by_name(data_sec):
     clean_input = clean_input.lower()
     clean_input = clean_input.translate(str.maketrans('', '', string.punctuation + u'\xa0'))
     clean_input = clean_input.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), ''))
-    # print(recipe_name)
 
-    list = []
-    for j, row in recipeName.iterrows():
-        if recipeName.at[j, 'Title'] == clean_input:
-            list.append([recipeName.at[j, 'Instructions']])
-    list_df = pd.DataFrame(list, columns=['Instructions'])
-    list_df = list_df.sort_values('Instructions')
-
+    vectorizer = TfidfVectorizer(ngram_range=(1, 2))
+    X = vectorizer.fit_transform(recipeName['Title'])
+    query_vec = vectorizer.transform([clean_input])
+    results = cosine_similarity(X, query_vec).reshape((-1,))
     count = 0
-    for i, row in list_df.iterrows():
+    query = []
+    for i in results.argsort()[-10:][::-1]:
+        title = recipeName.iloc[i, 1]
+        recipes = recipeName.iloc[i, 3]
         count += 1
-        print("Recipes:", list_df.at[i, 'Instructions'])
+        query.append([title, recipes])
+    querydf = pd.DataFrame(query, columns=["Title", "Recipes"])
+    print(querydf)
+
 
 
 if __name__ == '__main__':
