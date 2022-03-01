865c24edd212b3562560b5413e676bcb28e7259f
mineteeraa <patteera_mine@hotmail.com>
2022-03-01 17:31:03 +0700

    Add spellCheker
    Add spellChecker to check the input is correct or not

--- search_recipes.py
+++ search_recipes.py
@@ -1,21 +1,40 @@
+import re
 import string
-
 import numpy as np
 import pandas as pd
 
+from spellchecker import SpellChecker
+
+spell = SpellChecker()
+
+
+# def get_and_clean():
+#     readFile = pd.read_csv('resource/Food Ingredients and Recipe Dataset with Image Name Mapping.csv')
+#     readFile = pd.DataFrame(readFile)
+#     search_for_recipe_by_name(readFile)
 
 def get_and_clean():
     readFile = pd.read_csv('resource/Food Ingredients and Recipe Dataset with Image Name Mapping.csv')
     readFile = pd.DataFrame(readFile)
-    search_for_recipe_by_name(readFile)
+    readFile = readFile.dropna()
+    readFile = readFile.drop_duplicates()
+    for i, row in readFile.iterrows():
+        readFile.at[i, 'Title'] = readFile.at[i, 'Title'].lower()
+        readFile.at[i, 'Title'] = readFile.at[i, 'Title'].translate(str.maketrans('', '', string.punctuation + u'\xa0'))
+        readFile.at[i, 'Title'] = readFile.at[i, 'Title'].translate(
+            str.maketrans(string.whitespace, ' ' * len(string.whitespace), ''))
 
-def search_for_recipe_by_name(data_sec):
+    search_for_recipe_by_name_TFIDF(readFile)
+
+
+def search_for_recipe_by_name_TFIDF(data_sec):
     recipeName = data_sec
     for i, row in recipeName.iterrows():
         recipeName.at[i, 'Title'] = recipeName.at[i, 'Title'].lower()
-        recipeName.at[i, 'Title'] = recipeName.at[i, 'Title'].translate(str.maketrans('', '', string.punctuation + u'\xa0'))
-        recipeName.at[i, 'Title'] = recipeName.at[i, 'Title'].translate(str.maketrans(string.whitespace, ' '*len(string.whitespace), ''))
-
+        recipeName.at[i, 'Title'] = recipeName.at[i, 'Title'].translate(
+            str.maketrans('', '', string.punctuation + u'\xa0'))
+        recipeName.at[i, 'Title'] = recipeName.at[i, 'Title'].translate(
+            str.maketrans(string.whitespace, ' ' * len(string.whitespace), ''))
 
     print("-------------------------------------------")
     print("input name: ")
@@ -24,6 +43,8 @@ def search_for_recipe_by_name(data_sec):
     clean_input = clean_input.lower()
     clean_input = clean_input.translate(str.maketrans('', '', string.punctuation + u'\xa0'))
     clean_input = clean_input.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), ''))
+    spell_correct = spell.correction(clean_input)
+    print({'Recipe name': clean_input, 'Spell correct': ' '.join(spell_correct)})
     # print(recipe_name)
 
     list = []
