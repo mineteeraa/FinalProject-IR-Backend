a441d50ba9fa764581199d65b2bb8087f22371e7
mineteeraa <patteera_mine@hotmail.com>
2022-03-04 16:05:11 +0700

    Create function to search recipe from favourite
    Create search_favourite in main.py and search_recipe_from_favourite in search_receiptes.py to search favourite recipe

--- auth/project/main.py
+++ auth/project/main.py
@@ -1,9 +1,10 @@
+import pandas as pd
 import requests
 from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 
 from .models import Favourite
-from .search_recipes import search_for_recipe_by_ingredients_TFIDF
+from .search_recipes import search_for_recipe_by_ingredients_TFIDF, search_recipe_from_favourite
 from . import data_sec
 
 main = Blueprint('main', __name__)
@@ -34,3 +35,15 @@ def favourite():
     favourite = Favourite.query.filter(Favourite.userid == current_user.id).all()
     return render_template('favouritelist.html', data=favourite, userid=current_user.id)
 
+
+@main.route('/searchFavourite', methods=['POST'])
+@login_required
+def search_favourite():
+    search_fav = request.form.get("searchFavourite")
+    favourite = Favourite.query.filter(Favourite.userid == current_user.id).all()
+    favourite_list = []
+    for fav in favourite:
+        favourite_list.append([fav.id, fav.title, fav.image])
+    df = pd.DataFrame(favourite_list, columns=['id', 'title', 'image'])
+    data_favourite = search_recipe_from_favourite(df, search_fav)
+    return render_template('favouritelist.html', data=data_favourite, userid=current_user.id)
--- auth/project/search_recipes.py
+++ auth/project/search_recipes.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import re
 
+
 def get_and_clean():
     readFile = pd.read_csv('../resource/Food Ingredients and Recipe Dataset with Image Name Mapping.csv')
     readFile = pd.DataFrame(readFile)
@@ -13,8 +14,10 @@ def get_and_clean():
     readFile = readFile.drop_duplicates()
     for i, row in readFile.iterrows():
         readFile.at[i, 'Instructions'] = readFile.at[i, 'Instructions'].lower()
-        readFile.at[i, 'Instructions'] = readFile.at[i, 'Instructions'].translate(str.maketrans('', '', string.punctuation + u'\xa0'))
-        readFile.at[i, 'Instructions'] = readFile.at[i, 'Instructions'].translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), ''))
+        readFile.at[i, 'Instructions'] = readFile.at[i, 'Instructions'].translate(
+            str.maketrans('', '', string.punctuation + u'\xa0'))
+        readFile.at[i, 'Instructions'] = readFile.at[i, 'Instructions'].translate(
+            str.maketrans(string.whitespace, ' ' * len(string.whitespace), ''))
 
     return readFile
 
@@ -23,8 +26,10 @@ def search_for_recipe_by_ingredients_TFIDF(data_sec, ingredients):
     ingredientsName = data_sec
     for i, row in ingredientsName.iterrows():
         ingredientsName.at[i, 'Cleaned_Ingredients'] = ingredientsName.at[i, 'Cleaned_Ingredients'].lower()
-        ingredientsName.at[i, 'Cleaned_Ingredients'] = ingredientsName.at[i, 'Cleaned_Ingredients'].translate(str.maketrans('', '', string.punctuation + u'\xa0'))
-        ingredientsName.at[i, 'Cleaned_Ingredients'] = ingredientsName.at[i, 'Cleaned_Ingredients'].translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), ''))
+        ingredientsName.at[i, 'Cleaned_Ingredients'] = ingredientsName.at[i, 'Cleaned_Ingredients'].translate(
+            str.maketrans('', '', string.punctuation + u'\xa0'))
+        ingredientsName.at[i, 'Cleaned_Ingredients'] = ingredientsName.at[i, 'Cleaned_Ingredients'].translate(
+            str.maketrans(string.whitespace, ' ' * len(string.whitespace), ''))
 
     print("-------------------------------------------")
     print("input ingredients: ")
@@ -41,14 +46,33 @@ def search_for_recipe_by_ingredients_TFIDF(data_sec, ingredients):
     query = []
     for i in results.argsort()[-5:][::-1]:
         query.append({
-                      "Food_name": ingredientsName.at[i, 'Title'],
-                      "Ingredients": ingredientsName.at[i, 'Cleaned_Ingredients'],
-                      "Recipes": ingredientsName.at[i, 'Instructions'],
-                      "Images" : ingredientsName.at[i, 'Image_Name'] + ".jpg"
-                    })
+            "Food_name": ingredientsName.at[i, 'Title'],
+            "Ingredients": ingredientsName.at[i, 'Cleaned_Ingredients'],
+            "Recipes": ingredientsName.at[i, 'Instructions'],
+            "Images": ingredientsName.at[i, 'Image_Name'] + ".jpg"
+        })
     return query
 
 
+def search_recipe_from_favourite(data_sec, favouriteInput):
+    favouriteRecipe = data_sec
+    clean_input = favouriteInput
+    clean_input = clean_input.lower()
+    clean_input = clean_input.translate(str.maketrans('', '', string.punctuation + u'\xa0'))
+    clean_input = clean_input.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), ''))
+    vectorizer = TfidfVectorizer(ngram_range=(1, 2))
+    X = vectorizer.fit_transform(favouriteRecipe['title'])
+    query_vec = vectorizer.transform([clean_input])
+    results = cosine_similarity(X, query_vec).reshape((-1,))
+    query = []
+    for i in results.argsort()[::-1]:
+        if results[i] > 0:
+            query.append({
+                "id": favouriteRecipe.at[i, 'id'],
+                "title": favouriteRecipe.at[i, 'title'],
+                "image": favouriteRecipe.at[i, 'image']
+            })
+    return query
 
 
 if __name__ == '__main__':
